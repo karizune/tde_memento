1dc24d2b8ff800acb56580814656828423537a5f
karizune <luanchrystian2@gmail.com>
2022-06-14 21:24:03 -0300

    Add project files.

---
+++ trabalho sobre memento/Modelos/Pessoa.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace trabalho_sobre_memento.Modelos
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public Pessoa Memento { get; private set; }
+
+        public Pessoa(string nome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+        }
+
+        //salvar estado, guardar informações de nome e idade. criando uma nova instancia do objeto
+        public void SalvarEstado()
+        {
+            Memento = new Pessoa(Nome, Idade);
+        }
+
+        //restaurar estado, recuperar informações do memento e passar para a classe atual
+        public void RestaurarEstado()
+        {
+            if (Memento != null)
+            {
+                Nome = Memento.Nome;
+                Idade = Memento.Idade;
+            }
+        }
+
+        public void MostrarDetalhes()
+        {
+            Console.WriteLine($"Pessoa: Nome é {Nome} e idade é {Idade}");
+
+            if (Memento != null)
+            {
+                Console.WriteLine($"Memento: Nome é {Memento.Nome} e idade é {Memento.Idade}");
+            }
+        }
+    }
+}
---
+++ trabalho sobre memento/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using trabalho_sobre_memento.Modelos;
+
+namespace trabalho_sobre_memento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Criar a instância de pessoa
+            Pessoa pessoa = new Pessoa("Gabrielle",23);
+
+            //Salvar estado
+            pessoa.SalvarEstado();
+
+            //Mostrar situação atual e do memento
+            pessoa.MostrarDetalhes();
+
+            //Alterar atributos
+            pessoa.Nome = "Luan";
+            pessoa.Idade = 23;
+
+            //Mostrar situação atual e do memento
+            Console.WriteLine();
+            pessoa.MostrarDetalhes();
+
+            //Restaurar estado
+            pessoa.RestaurarEstado();
+
+            //Mostrar situação atual e do memento
+            Console.WriteLine();
+            pessoa.MostrarDetalhes();
+
+            Console.ReadKey();
+        }
+    }
+}
